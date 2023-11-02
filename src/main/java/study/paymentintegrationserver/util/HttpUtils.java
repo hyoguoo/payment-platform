b6d50d76c3f7a275024d210d63a8348049c87c49
hyoguoo <hyoguoo@gmail.com>
2023-11-03 01:08:42 +0900

    feat: Basic Authorization POST 요청 HTTP util 추가

---
+++ src/main/java/study/paymentintegrationserver/util/HttpUtils.java
@@ -0,0 +1,34 @@
+package study.paymentintegrationserver.util;
+
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+
+public final class HttpUtils {
+
+    private static final String AUTHORIZATION_HEADER_NAME = "Authorization";
+    private static final String BASIC_AUTHORIZATION_TYPE = "Basic ";
+
+    private HttpUtils() {
+        throw new AssertionError();
+    }
+
+    public static <T> T requestPostWithBasicAuthorization(String url, String authorization, T body, Class<T> responseType) {
+        HttpEntity<T> request = generateBasicAuthorizationHttpEntity(authorization, body);
+        return new RestTemplate().postForObject(url, request, responseType);
+    }
+
+    private static <T> HttpEntity<T> generateBasicAuthorizationHttpEntity(String authorization, T body) {
+        return new HttpEntity<>(body, generateBasicAuthorizationHttpHeaders(authorization));
+    }
+
+    private static HttpHeaders generateBasicAuthorizationHttpHeaders(String authorization) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(AUTHORIZATION_HEADER_NAME, BASIC_AUTHORIZATION_TYPE + authorization);
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        return httpHeaders;
+    }
+}
