f3c8a636ed492d4996cfb74ced9ee8877e4761fc
hyoguoo <hyoguoo@gmail.com>
2023-11-03 15:53:18 +0900

    feat: OrderCreateRequest To Entity 메서드 추가

--- src/main/java/study/paymentintegrationserver/dto/order/OrderCreateRequest.java
+++ src/main/java/study/paymentintegrationserver/dto/order/OrderCreateRequest.java
@@ -2,6 +2,9 @@ package study.paymentintegrationserver.dto.order;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import study.paymentintegrationserver.entity.OrderInfo;
+import study.paymentintegrationserver.entity.Product;
+import study.paymentintegrationserver.entity.User;
 
 import java.math.BigDecimal;
 
@@ -13,4 +16,14 @@ public class OrderCreateRequest {
     private final String orderId;
     private final BigDecimal amount;
     private final OrderProduct orderProduct;
+
+    public OrderInfo toEntity(User user, Product product) {
+        return OrderInfo.builder()
+                .user(user)
+                .product(product)
+                .orderId(this.orderId)
+                .quantity(this.orderProduct.getQuantity())
+                .totalAmount(this.amount)
+                .build();
+    }
 }
