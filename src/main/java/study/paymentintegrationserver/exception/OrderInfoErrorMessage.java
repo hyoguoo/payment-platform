93dc6888a7a1d0af4117f40bdf0c7caba06e2658
hyoguoo <hyoguoo@gmail.com>
2023-11-03 22:02:57 +0900

    feat: 주문 생성 관련 에러 메시지 추가

--- src/main/java/study/paymentintegrationserver/exception/OrderInfoErrorMessage.java
+++ src/main/java/study/paymentintegrationserver/exception/OrderInfoErrorMessage.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum OrderInfoErrorMessage {
 
-    NOT_FOUND("Not exist order id");
+    NOT_FOUND("Not exist order id"),
+    INVALID_TOTAL_AMOUNT("Invalid total amount");
 
     private final String message;
 
--- src/main/java/study/paymentintegrationserver/exception/ProductErrorMessage.java
+++ src/main/java/study/paymentintegrationserver/exception/ProductErrorMessage.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 public enum ProductErrorMessage {
 
     NOT_FOUND("Not exist product id"),
-    NOT_ENOUGH_STOCK("Not enough stock");
+    NOT_ENOUGH_STOCK("Not enough stock"),
+    NOT_CORRECT_PRODUCT("Not correct product");
 
     private final String message;
 
