6633425f16baa756de6c21937c5a91a46746d48c
hyoguoo <hyoguoo@gmail.com>
2024-09-10 01:13:24 +0900

    feat: Payment DTO 변환 매퍼 클래스 추가

---
+++ src/main/java/com/hyoguoo/paymentplatform/payment/infrasturcture/PaymentInfrastructureMapper.java
@@ -0,0 +1,23 @@
+package com.hyoguoo.paymentplatform.payment.infrasturcture;
+
+import com.hyoguoo.paymentplatform.payment.application.dto.response.TossPaymentDetails;
+import com.hyoguoo.paymentplatform.payment.infrasturcture.dto.response.TossPaymentResponse;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PaymentInfrastructureMapper {
+
+    public static TossPaymentDetails toPaymentDetails(TossPaymentResponse tossPaymentResponse) {
+        return TossPaymentDetails.builder()
+                .paymentKey(tossPaymentResponse.getPaymentKey())
+                .orderName(tossPaymentResponse.getOrderName())
+                .method(tossPaymentResponse.getMethod())
+                .totalAmount(tossPaymentResponse.getTotalAmount())
+                .status(tossPaymentResponse.getStatus())
+                .requestedAt(tossPaymentResponse.getRequestedAt())
+                .approvedAt(tossPaymentResponse.getApprovedAt())
+                .lastTransactionKey(tossPaymentResponse.getLastTransactionKey())
+                .build();
+    }
+}
