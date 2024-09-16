5f6b02974f5f76428a005fa7a5f62e6f129444c5
hyoguoo <hyoguoo@gmail.com>
2024-09-17 03:07:48 +0900

    chore: 기본 상품 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,7 @@ WHERE NOT EXISTS (SELECT * FROM user WHERE id = 2);
 INSERT INTO product (id, created_at, updated_at, description, name, price, stock, seller_id)
 SELECT 1, '2023-01-01 00:00:00', '2023-01-01 00:00:00', 'Ogu Platypus', 'Ogu T', 50000, 59, 2
 WHERE NOT EXISTS (SELECT * FROM product WHERE id = 1);
+
+INSERT INTO product (id, created_at, updated_at, description, name, price, stock, seller_id)
+SELECT 2, '2023-01-01 00:00:00', '2023-01-01 00:00:00', 'Ogu Platypus', 'Baby Ogu T', 30000, 59, 2
+WHERE NOT EXISTS (SELECT * FROM product WHERE id = 2);
