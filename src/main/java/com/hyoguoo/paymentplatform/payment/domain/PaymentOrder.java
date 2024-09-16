0b43f7c9abed042e28b268f293c31801c397287b
hyoguoo <hyoguoo@gmail.com>
2024-09-17 03:01:12 +0900

    mod: 불필요하게 총금액 검증하는 로직 삭제

--- src/main/java/com/hyoguoo/paymentplatform/payment/domain/PaymentOrder.java
+++ src/main/java/com/hyoguoo/paymentplatform/payment/domain/PaymentOrder.java
@@ -3,8 +3,6 @@ package com.hyoguoo.paymentplatform.payment.domain;
 import com.hyoguoo.paymentplatform.payment.application.dto.vo.OrderedProduct;
 import com.hyoguoo.paymentplatform.payment.domain.dto.ProductInfo;
 import com.hyoguoo.paymentplatform.payment.domain.enums.PaymentOrderStatus;
-import com.hyoguoo.paymentplatform.payment.exception.PaymentValidException;
-import com.hyoguoo.paymentplatform.payment.exception.common.PaymentErrorCode;
 import java.math.BigDecimal;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -34,22 +32,9 @@ public class PaymentOrder {
         this.orderId = paymentEvent.getOrderId();
         this.productId = orderedProduct.getProductId();
         this.quantity = orderedProduct.getQuantity();
-        this.totalAmount = productInfo.getPrice().multiply(BigDecimal.valueOf(orderedProduct.getQuantity()));
-
-        this.status = PaymentOrderStatus.NOT_STARTED;
-
-        validateTotalAmount(productInfo, orderedProduct);
-    }
-
-    private void validateTotalAmount(
-            ProductInfo productInfo,
-            OrderedProduct orderedProduct
-    ) {
-        BigDecimal calculatedAmount = productInfo.getPrice()
+        this.totalAmount = productInfo.getPrice()
                 .multiply(BigDecimal.valueOf(orderedProduct.getQuantity()));
 
-        if (calculatedAmount.compareTo(this.totalAmount) != 0) {
-            throw PaymentValidException.of(PaymentErrorCode.INVALID_TOTAL_AMOUNT);
-        }
+        this.status = PaymentOrderStatus.NOT_STARTED;
     }
 }
