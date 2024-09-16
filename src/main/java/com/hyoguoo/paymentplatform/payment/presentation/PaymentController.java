a467760b35032439fd88617a69f42ff49b551c8c
hyoguoo <hyoguoo@gmail.com>
2024-09-17 03:21:48 +0900

    feat: checkout api 연결

--- src/main/java/com/hyoguoo/paymentplatform/payment/presentation/PaymentController.java
+++ src/main/java/com/hyoguoo/paymentplatform/payment/presentation/PaymentController.java
@@ -1,9 +1,12 @@
 package com.hyoguoo.paymentplatform.payment.presentation;
 
-import com.hyoguoo.paymentplatform.payment.presentation.dto.request.PaymentConfirmRequest;
+import com.hyoguoo.paymentplatform.payment.application.dto.request.CheckoutCommand;
+import com.hyoguoo.paymentplatform.payment.application.dto.response.CheckoutResult;
 import com.hyoguoo.paymentplatform.payment.presentation.dto.request.CheckoutRequest;
-import com.hyoguoo.paymentplatform.payment.presentation.dto.response.PaymentConfirmResponse;
+import com.hyoguoo.paymentplatform.payment.presentation.dto.request.PaymentConfirmRequest;
 import com.hyoguoo.paymentplatform.payment.presentation.dto.response.CheckoutResponse;
+import com.hyoguoo.paymentplatform.payment.presentation.dto.response.PaymentConfirmResponse;
+import com.hyoguoo.paymentplatform.payment.presentation.port.PaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,11 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class PaymentController {
 
+    private final PaymentService paymentService;
+
     @PostMapping("/api/v1/payments/checkout")
     public CheckoutResponse checkout(
             @RequestBody CheckoutRequest checkoutRequest
     ) {
-        return CheckoutResponse.builder().build();
+        CheckoutCommand checkoutCommand = PaymentPresentationMapper.toCheckoutCommand(
+                checkoutRequest
+        );
+        CheckoutResult checkoutResult = paymentService.checkout(checkoutCommand);
+
+        return PaymentPresentationMapper.toCheckoutResponse(checkoutResult);
     }
 
     @PostMapping("/api/v1/payments/confirm")
