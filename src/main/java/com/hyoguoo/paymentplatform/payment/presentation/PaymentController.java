af18f4fe088c1383f0abb0019deec637c317c1db
hyoguoo <hyoguoo@gmail.com>
2024-09-17 08:56:49 +0900

    feat: 결제 승인 API 추가

--- src/main/java/com/hyoguoo/paymentplatform/payment/presentation/PaymentController.java
+++ src/main/java/com/hyoguoo/paymentplatform/payment/presentation/PaymentController.java
@@ -1,12 +1,15 @@
 package com.hyoguoo.paymentplatform.payment.presentation;
 
 import com.hyoguoo.paymentplatform.payment.application.dto.request.CheckoutCommand;
+import com.hyoguoo.paymentplatform.payment.application.dto.request.PaymentConfirmCommand;
 import com.hyoguoo.paymentplatform.payment.application.dto.response.CheckoutResult;
+import com.hyoguoo.paymentplatform.payment.application.dto.response.PaymentConfirmResult;
 import com.hyoguoo.paymentplatform.payment.presentation.dto.request.CheckoutRequest;
 import com.hyoguoo.paymentplatform.payment.presentation.dto.request.PaymentConfirmRequest;
 import com.hyoguoo.paymentplatform.payment.presentation.dto.response.CheckoutResponse;
 import com.hyoguoo.paymentplatform.payment.presentation.dto.response.PaymentConfirmResponse;
 import com.hyoguoo.paymentplatform.payment.presentation.port.PaymentCheckoutService;
+import com.hyoguoo.paymentplatform.payment.presentation.port.PaymentConfirmService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentController {
 
     private final PaymentCheckoutService paymentCheckoutService;
+    private final PaymentConfirmService paymentConfirmService;
 
     @PostMapping("/api/v1/payments/checkout")
     public CheckoutResponse checkout(
@@ -34,6 +38,11 @@ public class PaymentController {
     public PaymentConfirmResponse confirm(
             @RequestBody PaymentConfirmRequest paymentConfirmRequest
     ) {
-        return PaymentConfirmResponse.builder().build();
+        PaymentConfirmCommand paymentConfirmCommand = PaymentPresentationMapper.toPaymentConfirmCommand(
+                paymentConfirmRequest
+        );
+        PaymentConfirmResult paymentConfirmResult = paymentConfirmService.confirm(paymentConfirmCommand);
+
+        return PaymentPresentationMapper.toPaymentConfirmResponse(paymentConfirmResult);
     }
 }
--- src/main/java/com/hyoguoo/paymentplatform/payment/presentation/PaymentPresentationMapper.java
+++ src/main/java/com/hyoguoo/paymentplatform/payment/presentation/PaymentPresentationMapper.java
@@ -1,9 +1,13 @@
 package com.hyoguoo.paymentplatform.payment.presentation;
 
 import com.hyoguoo.paymentplatform.payment.application.dto.request.CheckoutCommand;
+import com.hyoguoo.paymentplatform.payment.application.dto.request.PaymentConfirmCommand;
 import com.hyoguoo.paymentplatform.payment.application.dto.response.CheckoutResult;
+import com.hyoguoo.paymentplatform.payment.application.dto.response.PaymentConfirmResult;
 import com.hyoguoo.paymentplatform.payment.presentation.dto.request.CheckoutRequest;
+import com.hyoguoo.paymentplatform.payment.presentation.dto.request.PaymentConfirmRequest;
 import com.hyoguoo.paymentplatform.payment.presentation.dto.response.CheckoutResponse;
+import com.hyoguoo.paymentplatform.payment.presentation.dto.response.PaymentConfirmResponse;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -21,6 +25,26 @@ public class PaymentPresentationMapper {
         return CheckoutResponse.builder()
                 .orderId(result.getOrderId())
                 .totalAmount(result.getTotalAmount())
+                .build();
+    }
+
+    public static PaymentConfirmCommand toPaymentConfirmCommand(
+            PaymentConfirmRequest paymentConfirmRequest
+    ) {
+        return PaymentConfirmCommand.builder()
+                .userId(paymentConfirmRequest.getUserId())
+                .orderId(paymentConfirmRequest.getOrderId())
+                .paymentKey(paymentConfirmRequest.getPaymentKey())
+                .amount(paymentConfirmRequest.getAmount())
+                .build();
+    }
+
+    public static PaymentConfirmResponse toPaymentConfirmResponse(
+            PaymentConfirmResult paymentConfirmResult
+    ) {
+        return PaymentConfirmResponse.builder()
+                .orderId(paymentConfirmResult.getOrderId())
+                .amount(paymentConfirmResult.getAmount())
                 .build();
     }
 }
