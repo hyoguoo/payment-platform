e4f7f72d715a51fd8091a70cd5cb1c298cc86e89
hyoguoo <hyoguoo@gmail.com>
2024-09-17 02:30:07 +0900

    feat: payment event 생성 시 payment order 생성

--- src/main/java/com/hyoguoo/paymentplatform/payment/application/PaymentServiceImpl.java
+++ src/main/java/com/hyoguoo/paymentplatform/payment/application/PaymentServiceImpl.java
@@ -3,10 +3,15 @@ package com.hyoguoo.paymentplatform.payment.application;
 import com.hyoguoo.paymentplatform.core.common.service.port.LocalDateTimeProvider;
 import com.hyoguoo.paymentplatform.payment.application.dto.request.CheckoutCommand;
 import com.hyoguoo.paymentplatform.payment.application.dto.response.CheckoutResult;
+import com.hyoguoo.paymentplatform.payment.application.dto.vo.OrderProduct;
 import com.hyoguoo.paymentplatform.payment.application.port.PaymentEventRepository;
 import com.hyoguoo.paymentplatform.payment.application.port.ProductProvider;
 import com.hyoguoo.paymentplatform.payment.application.port.UserProvider;
 import com.hyoguoo.paymentplatform.payment.domain.PaymentEvent;
+import com.hyoguoo.paymentplatform.payment.domain.PaymentOrder;
+import com.hyoguoo.paymentplatform.payment.domain.dto.ProductInfo;
+import com.hyoguoo.paymentplatform.payment.domain.dto.UserInfo;
+import com.hyoguoo.paymentplatform.payment.infrastructure.repostitory.PaymentOrderRepository;
 import com.hyoguoo.paymentplatform.payment.presentation.port.PaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class PaymentServiceImpl implements PaymentService {
 
     private final PaymentEventRepository paymentEventRepository;
+    private final PaymentOrderRepository paymentOrderRepository;
     private final UserProvider userProvider;
     private final ProductProvider productProvider;
     private final LocalDateTimeProvider localDateTimeProvider;
@@ -24,21 +30,45 @@ public class PaymentServiceImpl implements PaymentService {
     @Override
     @Transactional
     public CheckoutResult checkout(CheckoutCommand request) {
-        PaymentEvent paymentEvent = PaymentEvent.requiredBuilder()
-                .userInfo(userProvider.getUserInfoById(request.getUserId()))
-                .productInfo(
-                        productProvider.getProductInfoById(
-                                request.getOrderProduct().getProductId()
-                        )
-                )
-                .checkoutCommand(request)
-                .now(localDateTimeProvider.now())
-                .requiredBuild();
+        UserInfo userInfoById = userProvider.getUserInfoById(request.getUserId());
+        ProductInfo productInfoById = productProvider.getProductInfoById(
+                request.getOrderProduct().getProductId()
+        );
 
-        PaymentEvent savedPaymentEvent = paymentEventRepository.saveOrUpdate(paymentEvent);
+        PaymentEvent savedPaymentEvent = savePaymentEvent(request, userInfoById, productInfoById);
+        savePaymentOrder(savedPaymentEvent, request.getOrderProduct(), productInfoById);
 
         return CheckoutResult.builder()
                 .orderId(savedPaymentEvent.getOrderId())
                 .build();
+    }
+
+    private PaymentEvent savePaymentEvent(
+            CheckoutCommand request,
+            UserInfo userInfoById,
+            ProductInfo productInfoById
+    ) {
+        PaymentEvent paymentEvent = PaymentEvent.requiredBuilder()
+                .userInfo(userInfoById)
+                .productInfo(productInfoById)
+                .checkoutCommand(request)
+                .now(localDateTimeProvider.now())
+                .requiredBuild();
+
+        return paymentEventRepository.saveOrUpdate(paymentEvent);
+    }
+
+    private PaymentOrder savePaymentOrder(
+            PaymentEvent savedPaymentEvent,
+            OrderProduct orderProduct,
+            ProductInfo productInfo
+    ) {
+        PaymentOrder paymentOrder = PaymentOrder.requiredBuilder()
+                .paymentEvent(savedPaymentEvent)
+                .orderProduct(orderProduct)
+                .productInfo(productInfo)
+                .requiredBuild();
+
+        return paymentOrderRepository.saveOrUpdate(paymentOrder);
     }
 }
