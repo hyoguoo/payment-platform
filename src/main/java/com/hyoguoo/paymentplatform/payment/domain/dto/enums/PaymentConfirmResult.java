5134a5aaf66b8fa9e1a32b4d720212a3727e49b7
hyoguoo <hyoguoo@gmail.com>
2024-09-24 18:56:36 +0900

    feat: enum value 값 추가

--- src/main/java/com/hyoguoo/paymentplatform/payment/domain/dto/enums/PaymentConfirmResult.java
+++ src/main/java/com/hyoguoo/paymentplatform/payment/domain/dto/enums/PaymentConfirmResult.java
@@ -1,12 +1,23 @@
 package com.hyoguoo.paymentplatform.payment.domain.dto.enums;
 
+import java.util.Arrays;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public enum PaymentConfirmResult {
-    SUCCESS,
-    RETRYABLE_FAILURE,
-    NON_RETRYABLE_FAILURE,
+    SUCCESS("SUCCESS"),
+    RETRYABLE_FAILURE("RETRYABLE_FAILURE"),
+    NON_RETRYABLE_FAILURE("NON_RETRYABLE_FAILURE"),
+    ;
+
+    private final String value;
+
+    public static PaymentConfirmResult of(String value) {
+        return Arrays.stream(PaymentConfirmResult.values())
+                .filter(paymentConfirmResult -> paymentConfirmResult.getValue().equals(value))
+                .findFirst()
+                .orElseThrow();
+    }
 }
--- src/main/java/com/hyoguoo/paymentplatform/paymentgateway/domain/enums/PaymentConfirmResult.java
+++ src/main/java/com/hyoguoo/paymentplatform/paymentgateway/domain/enums/PaymentConfirmResult.java
@@ -7,10 +7,12 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum PaymentConfirmResult {
-    SUCCESS,
-    RETRYABLE_FAILURE,
-    NON_RETRYABLE_FAILURE,
+    SUCCESS("SUCCESS"),
+    RETRYABLE_FAILURE("RETRYABLE_FAILURE"),
+    NON_RETRYABLE_FAILURE("NON_RETRYABLE_FAILURE"),
     ;
+
+    private final String value;
 
     public static PaymentConfirmResult of(TossPaymentErrorCode tossPaymentErrorCode) {
         if (tossPaymentErrorCode.isRetryableError()) {
