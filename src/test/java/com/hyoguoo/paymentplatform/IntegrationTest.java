3e1562ea75b8ad494a8905d3a3eead51830b5fd6
hyoguoo <hyoguoo@gmail.com>
2024-09-29 04:18:30 +0900

    test: MockMvc 테스트를 위한 Config 어노테이션 추가

--- src/test/java/com/hyoguoo/paymentplatform/IntegrationTest.java
+++ src/test/java/com/hyoguoo/paymentplatform/IntegrationTest.java
@@ -1,10 +1,12 @@
 package com.hyoguoo.paymentplatform;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@AutoConfigureMockMvc
 public abstract class IntegrationTest {
 
 }
