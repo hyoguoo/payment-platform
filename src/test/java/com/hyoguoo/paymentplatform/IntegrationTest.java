8c4608afbc2bd2c14ef1dd9637c465f7f792aac4
hyoguoo <hyoguoo@gmail.com>
2024-09-29 04:42:04 +0900

    test: 테스트 환경 설정 추가

--- src/test/java/com/hyoguoo/paymentplatform/IntegrationTest.java
+++ src/test/java/com/hyoguoo/paymentplatform/IntegrationTest.java
@@ -3,10 +3,14 @@ package com.hyoguoo.paymentplatform;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 
 @ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.yml")
+@Sql(scripts = "/data-test.sql")
 public abstract class IntegrationTest {
 
 }
