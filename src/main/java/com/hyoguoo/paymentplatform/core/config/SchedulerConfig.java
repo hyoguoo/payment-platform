2298798797a4ae3213dfdb3af0353af895c807c0
hyoguoo <hyoguoo@gmail.com>
2024-10-04 21:35:53 +0900

    feat: 스케줄러 실행을 위한 Config 클래스 추가

---
+++ src/main/java/com/hyoguoo/paymentplatform/core/config/SchedulerConfig.java
@@ -0,0 +1,10 @@
+package com.hyoguoo.paymentplatform.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+
+}
