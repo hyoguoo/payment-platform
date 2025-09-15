3eea1edce999d397e622a5cd8bc429fbff5c59ac
hyoguoo <hyoguoo@gmail.com>
2025-09-15 20:06:07 +0900

    feat: PaymentHistory 생성 로직 개선 및 Factory 클래스 제거

--- src/main/java/com/hyoguoo/paymentplatform/payment/application/usecase/PaymentHistoryUseCase.java
+++ src/main/java/com/hyoguoo/paymentplatform/payment/application/usecase/PaymentHistoryUseCase.java
@@ -3,7 +3,6 @@ package com.hyoguoo.paymentplatform.payment.application.usecase;
 import com.hyoguoo.paymentplatform.payment.application.port.PaymentHistoryRepository;
 import com.hyoguoo.paymentplatform.payment.domain.PaymentHistory;
 import com.hyoguoo.paymentplatform.payment.domain.event.PaymentHistoryEvent;
-import com.hyoguoo.paymentplatform.payment.domain.factory.PaymentHistoryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,11 +12,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class PaymentHistoryUseCase {
 
     private final PaymentHistoryRepository paymentHistoryRepository;
-    private final PaymentHistoryFactory paymentHistoryFactory;
 
     @Transactional
     public void savePaymentHistory(PaymentHistoryEvent event) {
-        PaymentHistory paymentHistory = paymentHistoryFactory.createFromEvent(event);
+        PaymentHistory paymentHistory = PaymentHistory.from(event);
         paymentHistoryRepository.save(paymentHistory);
     }
 }
