6511022a3a4091dca50a774b46b269422087e2b8
hyoguoo <hyoguoo@gmail.com>
2026-03-17 12:28:16 +0900

    fix: Toss API 딜레이 미설정으로 전략 간 TPS 차이가 없던 문제 수정
    BenchmarkConfig에서 FakeTossHttpOperator 생성 시 딜레이를 주입하지 않아
    모든 전략의 응답 시간이 동일하게 측정됨.
    
    application-benchmark.yml에 fake.min/max-delay-millis(100~300ms) 추가,
    BenchmarkConfig에서 @Value로 바인딩하여 setDelayRange() 호출.

--- src/main/java/com/hyoguoo/paymentplatform/mock/BenchmarkConfig.java
+++ src/main/java/com/hyoguoo/paymentplatform/mock/BenchmarkConfig.java
@@ -1,6 +1,7 @@
 package com.hyoguoo.paymentplatform.mock;
 
 import com.hyoguoo.paymentplatform.core.common.infrastructure.http.HttpOperator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -12,7 +13,12 @@ public class BenchmarkConfig {
 
     @Bean
     @Primary
-    public HttpOperator httpOperator() {
-        return new FakeTossHttpOperator();
+    public HttpOperator httpOperator(
+            @Value("${spring.myapp.toss-payments.fake.min-delay-millis}") int minDelayMillis,
+            @Value("${spring.myapp.toss-payments.fake.max-delay-millis}") int maxDelayMillis
+    ) {
+        FakeTossHttpOperator op = new FakeTossHttpOperator();
+        op.setDelayRange(minDelayMillis, maxDelayMillis);
+        return op;
     }
 }
