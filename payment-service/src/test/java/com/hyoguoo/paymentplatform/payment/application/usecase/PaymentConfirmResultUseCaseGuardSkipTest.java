8a8208fe6efbd01b18596a13e1dd3e12ffdca6b8
hyoguoo <hyoguoo@gmail.com>
2026-06-11 20:32:50 +0900

    chore(payment): 미사용 import 제거

--- payment-service/src/test/java/com/hyoguoo/paymentplatform/payment/application/usecase/PaymentConfirmResultUseCaseGuardSkipTest.java
+++ payment-service/src/test/java/com/hyoguoo/paymentplatform/payment/application/usecase/PaymentConfirmResultUseCaseGuardSkipTest.java
@@ -23,7 +23,6 @@ import java.time.ZoneOffset;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.Mockito;
